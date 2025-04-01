53a4f93bced05f8e0e47a007e42e778414f6c24b
Diogo Pina <diogojpina@gmail.com>
2025-04-01 14:09:53 -0300

    Configure typeorm to use an sqlite3 database

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,32 @@ import { AppService } from './app.service';
 import { FinancialModule } from './financial/financial.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { configuration } from './config/configutation';
 
 @Module({
-  imports: [FinancialModule, UserModule, AuthModule],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [
+        ConfigModule.forRoot({
+          load: [configuration],
+        }),
+      ],
+      useFactory: (configService: ConfigService) => {
+        return {
+          type: 'sqlite',
+          database: configService.get<string>('database.path'),
+          autoLoadEntities: true,
+          synchronize: true,
+        };
+      },
+      inject: [ConfigService],
+    }),
+    FinancialModule,
+    UserModule,
+    AuthModule,
+  ],
   controllers: [],
   providers: [AppService],
 })
