d6e8556529e84767ed5d10c891bedb38474e046c
Diogo Pina <diogojpina@gmail.com>
2025-04-01 14:43:49 -0300

    Clean unused financial controller and service

--- src/financial/financial.module.ts
+++ src/financial/financial.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { FinancialController } from './controllers/financial.controller';
-import { FinancialService } from './services/financial.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ManagementGroup } from './entities/management.group';
 import { Expense } from './entities/expense';
@@ -10,7 +8,7 @@ import { ManagementGroupService } from './services/management.group.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([ManagementGroup, Expense, ExpenseSplit])],
-  controllers: [FinancialController, ManagementGroupController],
-  providers: [FinancialService, ManagementGroupService],
+  controllers: [ManagementGroupController],
+  providers: [ManagementGroupService],
 })
 export class FinancialModule {}
